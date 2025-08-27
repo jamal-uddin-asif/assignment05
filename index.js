401e7aea73d80f27dd8cb981cacceb97d92d98ce
jamal-uddin-asif <asifzehendmg@gmail.com>
2025-08-27 23:24:14 +0600

    Heart button functionality added

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-console.log('assingment 5')
-console.log('so hard')
-console.log('i am here')
+// to get id 
+function getById (id){
+   let getId =  document.getElementById(id)
+   return getId;
+}
 
+
+// heart count section 
+const heartButtons = document.getElementsByClassName('btn-heart')
+console.log(heartButtons)
+
+for(let heartButton of heartButtons){
+    heartButton.addEventListener('click',function () {
+       const heartCount = getById('heart-count')
+       const heartCountInnetText = parseInt(heartCount.innerText)
+       const update = heartCountInnetText + 1;
+       getById('heart-count').innerText = update
+    })
+}
+
+
+
+// call button functionality 
+const callButton = document.getElementsByClassName('btn-call')
+console.log(callButton)
